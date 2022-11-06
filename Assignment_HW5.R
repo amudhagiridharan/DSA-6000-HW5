2de12de928011e971263f8e110be94a31cf3c20e
amudhagiridharan <99304173+amudhagiridharan@users.noreply.github.com>
2022-11-06 16:41:23 -0500

    Update Assignment_HW5.R
    added model averaging

--- Assignment_HW5.R
+++ Assignment_HW5.R
@@ -101,15 +101,21 @@ woe.binning.table(WOEstuffs2)
 woe.binning.plot(WOEstuffs2)
 
 ## define the train and test sets
+## as the data is imbalanced, let us sample equally from both categories
+
+flightcanc <- flight[(flight$Canceled == 1),]
+flightnoncanc <- flight[(flight$Canceled != 1),]
 set.seed(10)  # <-- group 10
-trainset <- sample(1:nrow(flight), 1000) 
-validset <- setdiff(1:nrow(flight), trainset) 
-testset<- flight[validset,]
-train<-flight[trainset,]
+
+
+train <- rbind(flightcanc[(sample(1:nrow(flightcanc), 600)),] , flightnoncanc[(sample(1:nrow(flightnoncanc), 600)),])
+testset<-setdiff(flight,train)
+
+
 
 ###########################################################################
 #Null model
-# The AUC observed is very less @0.48495
+# The AUC observed is very less @0.49001
 ###########################################################################
 m0 <- glm (Canceled ~ 1, data = train , family = binomial )
 summary(m0)
@@ -118,17 +124,17 @@ pred0 <- predict(m0,testset,type =  "response")
 # table(prob0, testset$Canceled)
 dff <- data.frame(label  = ifelse(testset$Canceled == "1", 1, 0),
                   m1 = pred0)
-AUCm0 <- ROC_func(dff, 1, 2, add_on = F, color = 0)
+AUCm0 <- ROC_func(dff, 1, 2, add_on = F, color = 1)
 cat(paste0("Null Model  AUC is ", ": ", format(AUCm0, digits = 5), "\n"))
 ###########################################################################
 # Full model
-# The AUC observed is quite high @0.69313  but ARR delay might have to be removed
+# The AUC observed is quite high @0.72416  but ARR delay might have to be removed
 ###########################################################################
 mfull <- glm (Canceled ~ . , data = train , family = binomial )
 predfull <- predict(mfull,testset,type =  "response")
 dff <- data.frame(label  = ifelse(testset$Canceled == "1", 1, 0),
                   m1 = predfull)
-AUCmfull <- ROC_func(dff, 1, 2, add_on = T, color = 1)
+AUCmfull <- ROC_func(dff, 1, 2, add_on = T, color = 2)
 cat(paste0("Model mfull  AUC is ", ": ", format(AUCmfull, digits = 5), "\n"))
 
 ###########################################################################
@@ -137,11 +143,12 @@ m1 <- glm (Canceled ~ .-ArrDelay , data = train , family = binomial )
 pred1 <- predict(m1,testset,type =  "response")
 dff <- data.frame(label  = ifelse(testset$Canceled == "1", 1, 0),
                   m1 = pred1)
-AUCm1 <- ROC_func(dff, 1, 2, add_on = T, color = 2)
+AUCm1 <- ROC_func(dff, 1, 2, add_on = T, color = 3)
 cat(paste0("Model  AUC is ", ": ", format(AUCm1, digits = 5), "\n"))
 
 ###########################################################################
 # we can observe that DepartureTime also is insignificant in the below model
+#AUC is 0.67546
 ###########################################################################
 m2 <- glm (Canceled ~ .-ArrDelay - DepDelay , data = train , family = binomial )
 summary(m2)
@@ -150,27 +157,27 @@ prob2 <- ifelse (pred2 > 0.23, 1,0)
 table(prob2, testset$Canceled)
 dff <- data.frame(label  = ifelse(testset$Canceled == "1", 1, 0),
                   m1 = pred2)
-AUCm2 <- ROC_func(dff, 1, 2, add_on = T, color = 3 )
+AUCm2 <- ROC_func(dff, 1, 2, add_on = T, color = 4 )
 cat(paste0("Model  AUC is ", ": ", format(AUCm2, digits = 5), "\n"))
 ###########################################################################
-# Model 3 - AUC 0.65372
+# Model 3 - AUC 0.66671
 ###########################################################################
 m3 <- glm (Canceled ~ .-ArrDelay - DepDelay - DepartureTime  , data = train , family = binomial )
 summary(m3)
-pred3 <- predict(m3,testset,type =  "response")
-dff <- data.frame(label  = ifelse(testset$Canceled == "1", 1, 0),
+pred3 <- predict(m3,diffset,type =  "response")
+dff <- data.frame(label  = ifelse(diffset$Canceled == "1", 1, 0),
                   m1 = pred3)
-AUCm3 <- ROC_func(dff, 1, 2, add_on = T, color = 4)
+AUCm3 <- ROC_func(dff, 1, 2, add_on = T, color = 5)
 cat(paste0("Model  AUC is ", ": ", format(AUCm3, digits = 5), "\n"))
 ###########################################################################
 # We see that AUC declines if we remove unique carrier. So we can choose M3 as the best model
 ###########################################################################
-m4 <- glm (Canceled ~ .-ArrDelay - DepDelay - DepartureTime - UniqueCarrier  , data = flight[trainset,] , family = binomial )
+m4 <- glm (Canceled ~ .-ArrDelay - DepDelay - DepartureTime - UniqueCarrier  , data = train , family = binomial )
 summary(m4)
 pred4 <- predict(m4,testset,type =  "response")
 dff <- data.frame(label  = ifelse(testset$Canceled == "1", 1, 0),
                   m1 = pred4)
-AUCm4 <- ROC_func(dff, 1, 2, add_on = T, color = 5)
+AUCm4 <- ROC_func(dff, 1, 2, add_on = T, color = 6)
 cat(paste0("Model  AUC is ", ": ", format(AUCm4, digits = 5), "\n"))
 ###########################################################################
 #Randomforest Model  AUC is : 0.65626
@@ -184,7 +191,7 @@ predrf <- predict(mrf,testset,type =  "response")
 #table(prob2, testset$Canceled)
 df <- data.frame(label  = ifelse(testset$Canceled == "1", 1, 0),
                  m1 = predrf)
-AUCrf <- ROC_func(df, 1, 2, add_on = T, color = 6)
+AUCrf <- ROC_func(df, 1, 2, add_on = T, color = 7)
 cat(paste0("Model  AUC is ", ": ", format(AUCrf, digits = 5), "\n"))
 
 ###########################################################################
@@ -196,9 +203,25 @@ modelnb1 <- naive_bayes(Canceled ~ Month + SchedElapsedTime + Distance+ UniqueCa
 prednb1 <- predict(modelnb1,test,type = "prob")
 dffnb1 <- data.frame(label  = ifelse(test$Canceled == "1", 1, 0),
                      m1 = ifelse(prednb1[,1] > prednb1[,2] , prednb1[,1] ,prednb1[,2]))
-AUCnb1 <- ROC_func(dffnb1, 1, 2, add_on = T, color = 7)
+AUCnb1 <- ROC_func(dffnb1, 1, 2, add_on = T, color = 8)
 cat(paste0("Naive bayes Model   AUC is ", ": ", format(AUCnb1, digits = 5), "\n"))
 
 
+# based on above anlaysis we choose Model 3, which excludes ARRDelay, DepDelay and DepartureTime
 ###########################################################################
-
+#Averaging model performace over various training and test sets
+###########################################################################
+modelcoeff  <- data.frame(matrix(ncol = 6, nrow = 0))
+for ( i in 1:100) {
+  set.seed(i)
+  # #define the train and test sets 
+  # train <- rbind(flightcanc[(sample(1:nrow(flightcanc), 600)),] , flightnoncanc[(sample(1:nrow(flightnoncanc), 600)),])
+  # testset<-setdiff(flight,train)
+  #Build the model
+  modelitr <- glm(Canceled ~ Month + SchedElapsedTime+ Distance+ UniqueCarrier, data = train ,family = binomial)
+  modelcoeff[i,] <- coefficients(modelitr)
+  cat(coefficients(modelitr))
+  cat("\n")
+}
+colnames(modelcoeff) <- c("Intercept", "Month" , "SchedElapsedTime", "Distance", "UniqueCarrierDL","UniqueCarrierUA" )
+summary(modelcoeff) # the average values are picked as func 1 coefficients
